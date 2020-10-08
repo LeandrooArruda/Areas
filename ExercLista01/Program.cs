60a03e289b9841c4f8ab1ef03674d4a826a05d5d
Leandro <leandropda96@gmail.com>
2020-10-08 20:32:41 -0300

    first commit

---
+++ ExercLista01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercLista01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double A, B, C, triangulo, circulo, trapezio, quadrado, retangulo;
+
+            string[] valores = Console.ReadLine().Split(' ');
+            A = double.Parse(valores[0], CultureInfo.InvariantCulture);
+            B = double.Parse(valores[1], CultureInfo.InvariantCulture);
+            C = double.Parse(valores[2], CultureInfo.InvariantCulture);
+
+            triangulo = A * C / 2.0;
+            circulo = Math.PI * Math.Pow(C, 2);
+            trapezio = (A + B) / 2.0 * C;
+            quadrado = Math.Pow(B, 2);
+            retangulo = A * B;
+
+            Console.WriteLine("TRIÂNGULO: " + triangulo.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("CIRCULO: " + circulo.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("TRAPÉZIO: " + trapezio.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("QUADRADO: " + quadrado.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("RETÂNGULO: " + retangulo.ToString("F3", CultureInfo.InvariantCulture));
+        }
+    }
+}
